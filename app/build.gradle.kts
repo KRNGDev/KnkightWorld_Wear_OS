db9bcb58143a547c9e783cb6a5848fc6d41384a0
Alberto Lopez Martinez <161486725+KRNGDev@users.noreply.github.com>
2025-09-30 16:59:18 +0200

    Add launcher icon resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,19 +55,21 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.wear.compose:compose-bom:1.2.0"))
-    implementation("androidx.wear.compose:compose-material")
-    implementation("androidx.wear.compose:compose-foundation")
-    implementation("androidx.wear.compose:compose-navigation")
+    val wearComposeVersion = "1.3.0"
+    implementation("androidx.wear.compose:compose-material:$wearComposeVersion")
+    implementation("androidx.wear.compose:compose-foundation:$wearComposeVersion")
+    implementation("androidx.wear.compose:compose-navigation:$wearComposeVersion")
 
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.wear:wear:1.3.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.3")
     implementation("androidx.compose.ui:ui:1.5.3")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.3")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.3")
